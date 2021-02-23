db06afa1598e701e04b2db35e27e9f6180adf094
rakshya2001 <bhattarakshya6@gmail.com>
2021-02-23 11:18:36 +0545

    Initial commit

---
+++ lab1.py
@@ -0,0 +1,6 @@
+#write a program that takes three numbers and prints their sum. Every number is given on a separate line
+val1= int(input('Enter first number'))
+val2= int(input('Enter second number'))
+val3= int(input('Enter third number'))
+Add= val1+val2+val3
+print('sum of 5three numbers is', Add)
