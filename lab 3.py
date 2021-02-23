c47bad2ea940144990c6551e0538334cf4fe3723
rakshya2001 <bhattarakshya6@gmail.com>
2021-02-23 12:22:29 +0545

    commit

--- lab 3.py
+++ lab 3.py
@@ -1,3 +1,9 @@
 # N students take K apples and distribute them among each other evenly. The remaining (the undivisible) part remains in
 # the basket. How many apples will each single student get? How many apples will remain in the basket?
 # The program reads the numbers N and K. It should print the two answers for the question above.
+N= int(input("Enter the number of students:"))
+K= int(input("Enter the number of apples:"))
+D= K/N
+R= K//N
+print(f'the number of apple got by the students are {D}')
+print(f'the apples left in the basket is{R}')
